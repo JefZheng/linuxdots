011fadef69c94e1de3ce0dd46770a21d5797867f
Jeffrey Zheng <jeffrey@smartrr.com>
2022-09-01 00:19:11 -0400

    Multimonitor polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,14 +3,13 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar i3poly &
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload i3poly &
+    MONITOR=$m polybar i3poly &
   done
 else
-  polybar --reload i3poly &
+  polybar i3poly &
 fi
 
 
